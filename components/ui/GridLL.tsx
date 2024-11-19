3937efe0583ab789b31c1ce571e59ad7d926ce89
Mayank <mayanksingh8181335335@gmail.com>
2024-11-19 23:53:41 +0530

    made some updates

--- components/ui/GridLL.tsx
+++ components/ui/GridLL.tsx
@@ -4,9 +4,14 @@ type PropsPavillion={
     pavillion: Array<boolean>;
 }
 
-function GridLL({pavillion}:PropsPavillion) {
+function GridLL() {
   return (
-    {}
+    <div className="grid grid-cols-2 grid-rows-2 bg-green-700">
+        <div className='bg-green-300 rounded-full m-[20%] border-green-900 border-4'></div>
+        <div className='bg-green-300 rounded-full m-[20%] border-green-900 border-4'></div>
+        <div className='bg-green-300 rounded-full m-[20%] border-green-900 border-4'></div>
+        <div className='bg-green-300 rounded-full m-[20%] border-green-900 border-4'></div>
+    </div>
   )
 }
 
--- components/ui/GridLR.tsx
+++ components/ui/GridLR.tsx
@@ -4,7 +4,12 @@ type PropsPavillion={
 }
 function GridLR({pavillion}:PropsPavillion) {
   return (
-    <div>GridLR</div>
+    <div className="grid grid-cols-2 grid-rows-2 bg-green-700">
+        <div className='bg-green-300 rounded-full m-[20%] border-green-900 border-4'></div>
+        <div className='bg-green-300 rounded-full m-[20%] border-green-900 border-4'></div>
+        <div className='bg-green-300 rounded-full m-[20%] border-green-900 border-4'></div>
+        <div className='bg-green-300 rounded-full m-[20%] border-green-900 border-4'></div>
+    </div>
   )
 }
 
